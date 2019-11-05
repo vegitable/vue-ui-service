f50b189ae850113d08d6fa84598c40495c52ea95
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-11-05 19:27:36 +0000

    adds navbar to menu screen

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -1,20 +1,43 @@
-<template>
-  <span id="link-background">
-    <NavBar
-      background="navbar is-link"
-      logoPassClass="title is-3 has-text-white-bis"
-      menuClass="icon is-large has-text-white-bis"
-    />
-  </span>
+<template id="link-background">
+  <section class="hero is-fullheight is-link">
+    <nav :class="background" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <router-link id="navbar-logo" class="navbar-item" to="/">
+          <Logo passClass="title is-3 has-text-white-bis" />
+        </router-link>
+
+        <a id="navbar-menu">
+          <router-link
+            id="menu-left-arrow"
+            class="icon is-large has-text-white-bis is-pulled-right"
+            to="/"
+          >
+            <i class="fa fa-lg fa-chevron-left"></i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+    <div class="hero-body">
+      <div>
+        <h1 class="title has-text-white-bis is-4">Add to home screen</h1>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import NavBar from '../components/NavBar.vue';
+import Logo from '../components/Logo.vue';
 
 export default {
   name: 'menuPage',
   components: {
-    NavBar,
+    Logo,
   },
 };
 </script>
+
+<style scoped>
+#menu-left-arrow {
+  padding: 4px 4px 0 0;
+}
+</style>
